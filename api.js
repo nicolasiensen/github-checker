6bf0b9266510546dadd08e874191a5163637cfa0
Nícolas Iensen <nicolas.iensen@gmail.com>
2020-05-01 12:04:53 +0200

    Add the API module

---
+++ api.js
@@ -0,0 +1,50 @@
+export async function loadPullRequests(userToken, username) {
+  return fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: { 'Authorization': `bearer ${userToken}` },
+    body: JSON.stringify(
+      {
+        query: `
+          {
+            search(
+              query: \"is:open is:pr review-requested:${username} archived:false\",
+              type: ISSUE,
+              first: 10
+            )
+            { issueCount }
+          }
+        `
+      }
+    )
+  }).then(response => {
+    if (response.status < 400) {
+      return response.json()
+    } else {
+      throw new Error(response.statusText)
+    }
+  })
+}
+
+export async function loadLogin(userToken) {
+  return fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: { 'Authorization': `bearer ${userToken}` },
+    body: JSON.stringify(
+      {
+        query: `
+          {
+            viewer {
+              login
+            }
+          }
+        `
+      }
+    )
+  }).then(response => {
+    if (response.status < 400) {
+      return response.json()
+    } else {
+      throw new Error(response.statusText)
+    }
+  })
+}
